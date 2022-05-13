d286d8ca2122bcf194aa0b278676241d902e0b07
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-13 16:30:53 -0400

    style: :lipstick: styles modal

--- src/Components/NewTransactionModal/newTransactionModal.tsx
+++ src/Components/NewTransactionModal/newTransactionModal.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import { Container } from './style';
+import closeImg from '../../assets/close.svg';
 
 interface NewTrasactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,27 @@ const NewTrasactionModal = ({
   onRequestClose
 }: NewTrasactionModalProps) => {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar Transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 };
