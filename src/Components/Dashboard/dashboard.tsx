e547b04f31f685969a46847bd56fbd6478690495
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-12 23:58:51 -0400

    feat: :zap: add TransactionsTable component on Dashboard

--- src/Components/Dashboard/dashboard.tsx
+++ src/Components/Dashboard/dashboard.tsx
@@ -1,10 +1,12 @@
 import Summary from '../Summary/summary';
+import TransactionsTable from '../TransactionsTable';
 import { Containter } from './style';
 
 const Dashboard = () => {
   return (
     <Containter>
       <Summary />
+      <TransactionsTable />
     </Containter>
   );
 };
