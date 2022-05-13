ed741d5a502402dad7913edefd87df4013f7191a
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-12 23:35:29 -0400

    feat: :zap: improve Header component organize

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,15 +1,1 @@
-import logoImg from '../../assets/logo.svg';
-import { Container, Content } from './style';
-
-const Header = () => {
-  return (
-    <Container>
-      <Content>
-        <img src={logoImg} alt="Pandas Financial" />
-        <button type="button">Nova Transação</button>
-      </Content>
-    </Container>
-  );
-};
-
-export default Header;
+export { default } from './header';
