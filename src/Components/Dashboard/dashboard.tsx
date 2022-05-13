ba115a1e8ff37d9b4b1fe5fad5bb950b47f4a2f0
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-12 23:34:52 -0400

    feat: :sparkles: add structure and styles Dashboard component

---
+++ src/Components/Dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import Summary from '../Summary/summary';
+import { Containter } from './style';
+
+const Dashboard = () => {
+  return (
+    <Containter>
+      <Summary />
+    </Containter>
+  );
+};
+
+export default Dashboard;
