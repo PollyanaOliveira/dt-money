f865f885adc17e85a668372854d655a28c986a92
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-13 14:35:08 -0400

    feat: :sparkles: add Modal on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import Dashboard from './Components/Dashboard';
 import Header from './Components/Header';
 import { GlobalStyle } from './styles';
 
+Modal.setAppElement('#root');
+
 const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,12 +1,17 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './style';
 
-const Header = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+const Header = ({ onOpenNewTransactionModal }: HeaderProps) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Pandas Financial" />
-        <button type="button">Nova Transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
   );
