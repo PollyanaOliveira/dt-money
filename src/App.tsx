511347cc4c582e8842e3d28a4fb3717ba7b614b7
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-12 23:33:14 -0400

    feat: :sparkles: add Dashboard component on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Dashboard from './Components/Dashboard';
 import Header from './Components/Header';
 import { GlobalStyle } from './styles';
 
@@ -5,6 +6,7 @@ const App = () => {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
